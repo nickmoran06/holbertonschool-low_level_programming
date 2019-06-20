359572232a3988f8083cda90c687ea85bcd49dad
nickmoran06 <956@holbertonschool.com>
2019-06-20 20:13:51 +0000

    print with char and numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char f;
+
+	for (f = '0'; f <= '9' ; f++)
+		putchar (f);
+	putchar('\n');
+	return (0);
+}
