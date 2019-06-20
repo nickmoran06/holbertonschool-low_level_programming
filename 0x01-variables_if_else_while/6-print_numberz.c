3af0bfab92dca310cad9bc1144c87d0e8356817a
nickmoran06 <956@holbertonschool.com>
2019-06-20 20:39:47 +0000

    print with ascii

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+		putchar (a);
+	putchar('\n');
+	return (0);
+}
