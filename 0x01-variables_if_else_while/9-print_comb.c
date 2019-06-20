fd0e6f3a97b43bcb9d89630c51caba8435d34013
nickmoran06 <956@holbertonschool.com>
2019-06-20 22:36:27 +0000

    for and if two conditions

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar (n);
+		if (n != 57)
+		{
+		putchar (44);
+		putchar (' ');
+		}
+	}
+putchar ('\n');
+return (0);
+}
