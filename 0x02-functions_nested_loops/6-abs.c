474ed05043a05f8132d00a7b9fc7a97eb5eadbe3
nickmoran06 <956@holbertonschool.com>
2019-06-24 23:49:21 +0000

    absolute value returnnnn

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _abs - convert to a positive number
+ *
+ * @a - The variable
+ */
+int _abs(int a)
+{
+	if (a >= 0)
+	{
+		return (a);
+	}
+	else
+	{
+		a = (a * -1);
+		return (a);
+	}
+}
