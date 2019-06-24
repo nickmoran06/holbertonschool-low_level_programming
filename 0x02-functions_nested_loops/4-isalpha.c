419ea46a390ed4b058ea3982ad0c6f9130bc3f28
nickmoran06 <956@holbertonschool.com>
2019-06-24 22:53:40 +0000

    identify all alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _isalpha - check for alphabetic character
+ *
+ * return 1 - if c is a letter, lowercase or uppercase
+ * return 0 - otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return(1);
+	}
+	else
+	{
+		return(0);
+	}
+	_putchar(10);
+}
