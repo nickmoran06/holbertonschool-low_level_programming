917f30ecde97a3b66bf69409697a55167fa66702
nickmoran06 <956@holbertonschool.com>
2019-06-25 23:27:19 +0000

    the god nine

--- 0x02-functions_nested_loops/.#9-times_table.c
+++
@@ -1,1 +0,0 @@
-vagrant@vagrant-ubuntu-trusty-64.6639:1561472476
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,11 +9,28 @@ void times_table(void)
 {
 	int line, colum;
 
-	for (line = 0, line <= 9; line++)
+	for (line = 0; line <= 9; line++)
 	{
 		for (colum = 0; colum <= 9; colum++)
 		{
-			line * colum
+			if (line * colum < 10)
+			{
+				if (colum != 0)
+				{
+					_putchar (44);
+					_putchar (32);
+					_putchar (32);
+				}
+				_putchar ((line * colum) % 10 + '0');
+			}
+			else
+			{
+				_putchar (44);
+				_putchar (32);
+				_putchar ((line * colum) / 10 + '0');
+                                _putchar ((line * colum) % 10 + '0');
+			}
 		}
+		_putchar(10);
 	}
 }
