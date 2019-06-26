99dc2efd28364289664b9fb10c437262436aa59e
nickmoran06 <956@holbertonschool.com>
2019-06-26 22:22:36 +0000

    all tables

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,9 +9,11 @@ void print_times_table(int n)
 {
 	int line, colum;
 
-	for (line = 0; line <= 9; line++)
+	if(n < 16 && n >= 0)
 	{
-		for (colum = 0; colum <= 9; colum++)
+	for (line = 0; line <= n; line++)
+	{
+		for (colum = 0; colum <= n; colum++)
 		{
 			if (line * colum < 10)
 			{
@@ -20,17 +22,28 @@ void print_times_table(int n)
 					_putchar (44);
 					_putchar (32);
 					_putchar (32);
+					_putchar (32);
 				}
 				_putchar ((line * colum) % 10 + '0');
+			}
+			else if (line * colum < 100)
+			{
+				_putchar (44);
+				_putchar (32);
+				_putchar (32);
+				_putchar ((line * colum) / 10 + '0');
+                                _putchar ((line * colum) % 10 + '0');
 			}
 			else
 			{
 				_putchar (44);
 				_putchar (32);
-				_putchar ((line * colum) / 10 + '0');
+				_putchar ((line * colum) / 100 + '0');
+				_putchar (((line * colum) / 10) % 10 + '0');
 				_putchar ((line * colum) % 10 + '0');
 			}
 		}
 		_putchar(10);
+	}
 	}
 }
