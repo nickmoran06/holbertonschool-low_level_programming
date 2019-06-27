a3e7cce6656b2cca8eae36b1cfb00f822e50437c
nickmoran06 <nicolasmoranpulido@gmail.com>
2019-06-27 04:41:36 +0000

    101 please

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,16 +3,18 @@
  * main - entry point
  *
  * Return: 0 success
+ *
  */
 int main(void)
 {
-  int n, Ans = 0;
+	int n, Ans;
 
-  for (n = 0; n < 1024; n++)
-    {
-      if (n % 3 == 0 || n % 5 == 0)
-	Ans += n;
-    }
-  printf("%d\n", Ans);
-  return (0);
+	n, Ans = 0;
+	for (n = 0; n < 1024; n++)
+	{
+		if (n % 3 == 0 || n % 5 == 0)
+			Ans += n;
+	}
+	printf("%d\n", Ans);
+	return (0);
 }
