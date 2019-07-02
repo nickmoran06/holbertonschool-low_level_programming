9a843e8b93bb1656b1c7172320b2b8a67e78521e
nickmoran06 <956@holbertonschool.com>
2019-07-02 20:33:33 +0000

    other logic

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,17 +1,21 @@
 #include "holberton.h"
 /**
  * puts2 - print one char out of 2
- *
  * @str: The string
  * Return: any
  */
 void puts2(char *str)
 {
-	int n = 0;
+	int n;
 
 	while (*(str + n))
 	{
-		_putchar (str[n]);
+		/*if(str[n] % 2 == 0)
+		{
+			_putchar (*(str + n));
+			n++;
+			}*/
+		_putchar(str[n]);
 		n = n + 2;
 	}
 	_putchar(10);
