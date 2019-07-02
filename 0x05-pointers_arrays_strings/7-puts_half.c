bc880fb7ea7a602fe33da67a1523665fcd3700fb
nickmoran06 <956@holbertonschool.com>
2019-07-02 21:22:46 +0000

    only know the synthax

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * puts_half - print the half of a string
+ * @str: the string
+ * Return: any
+ */
+void puts_half(char *str)
+{
+	int counter = 0, printer = 0;
+
+	while (*(str + counter))
+		counter++;
+	if (counter % 2 == 0)
+	{
+		printer = (counter / 2);
+	}
+	else
+	{
+		printer = (counter - 1) / 2;
+	}
+	while (*(str + printer))
+	{
+		_putchar(str[printer]);
+		printer++;
+	}
+	_putchar(10);
+}
