cb3a5b687a3cce5f890380cfcf96d149178d0304
nickmoran06 <956@holbertonschool.com>
2019-07-02 18:47:53 +0000

    arrays

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * rev_string - reverse a string
+ * @s: pointer
+ * Return: any
+ */
+void rev_string(char *s)
+{
+	int n = 0, d = 0;
+	char step[100];
+
+	while (*(s + d))
+	{
+		step[d] = s[d];
+		d++;
+	}
+	d = d - 1;
+	while (d >= 0)
+	{
+		s[d] = step[n];
+		n++;
+		d--;
+	}
+}
