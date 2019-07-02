02146d2efcc6c4dcbfd16de244cd4f34e5007d5f
nickmoran06 <956@holbertonschool.com>
2019-07-02 23:16:38 +0000

    keep it simple

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_array - print n elements of an array
+ * @a: pointer
+ * @n: variable
+ * Return: any
+ */
+void print_array(int *a, int n)
+{
+	int s;
+
+	for(s = 0; s < n; s++)
+	{
+		printf("%d", *(a + s));
+		if (s < (n - 1))
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
