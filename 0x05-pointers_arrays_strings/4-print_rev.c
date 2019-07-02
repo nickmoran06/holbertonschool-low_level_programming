e599109bab269520100d25c529fab53ae073e0cb
nickmoran06 <956@holbertonschool.com>
2019-07-02 15:32:45 +0000

    understanding synthaxis of pointers

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,12 +3,18 @@
  * print_rev - print a string in reverse
  * @s: pointer
  *
- * Return: 
+ * Return: any
  */
 void print_rev(char *s)
 {
-	int n;
+	int n = 0;
 
-	for (n = 0; n[s] <= 0; n--)
+	for (n = 0; n[s] != '\0'; n++)
+	{
+	}
+	while (n--)
+	{
 		_putchar (n[s]);
+	}
+	_putchar(10);
 }
