d4bc095498c4cca574a424107014b5f86f8ebc80
nickmoran06 <956@holbertonschool.com>
2019-07-05 02:35:00 +0000

    rot13

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * rot13 - encode a string using rot13
+ * @n: string
+ * Return: n success
+ */
+char *rot13(char *n)
+{
+	int s = 0, d;
+	char arr1[52] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char arr2[52] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	while (*(n + s))
+	{
+		for (d = 0; d <= 52; d++)
+		{
+			if (n[s] == arr1[d])
+				n[s] = arr2[d];
+		}
+		s++;
+	}
+	return (n);
+}
