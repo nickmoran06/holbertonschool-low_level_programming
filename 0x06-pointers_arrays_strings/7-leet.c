8d5b851a7e4685cbbd43bece80215f125c5f7128
nickmoran06 <nicolasmoranpulido@gmail.com>
2019-07-06 00:26:41 +0000

    until null

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,18 +6,17 @@
  */
 char *leet(char *n)
 {
-	int s = 0, d;
+	int s, d;
 	char arr1[10] = {97, 101, 111, 116, 73, 65, 69, 79, 84, 76};
 	char arr2[10] = {52, 51, 48, 55, 49, 52, 51, 48, 55, 49};
 
-	while (*(s + n) != 0)
+	for (s = 0; *(s + n) != 0; s++)
 	{
 		for (d = 0; d <= 9; d++)
 		{
 			if (n[s] == arr1[d])
 				n[s] = arr2[d];
 		}
-		s++;
 	}
 	return (n);
 }
