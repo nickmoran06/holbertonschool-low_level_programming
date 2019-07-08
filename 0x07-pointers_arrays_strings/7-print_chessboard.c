56b86f9c86f4d76b9f7b17858fc556bf231a6bd7
nickmoran06 <956@holbertonschool.com>
2019-07-08 22:17:13 +0000

    string de ajedrez

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,12 +1,22 @@
 #include "holberton.h"
 /**
  * print_chessboard - Write a function that prints the chessboard.
- * @a:
+ * @a: string of chess
  *
  *
- * Return:
+ * Return: void
  */
 void print_chessboard(char (*a)[8])
 {
+	int b = 0, c;
 
+	while (b < 8)
+	{
+		for (c = 0; c < 8; c++)
+		{
+			_putchar (a[b][c]);
+		}
+		_putchar (10);
+		b++;
+	}
 }
