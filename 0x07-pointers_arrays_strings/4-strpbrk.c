f1af2139d1c1109cdb9fe504fc43a9174d224c62
nickmoran06 <956@holbertonschool.com>
2019-07-08 21:50:51 +0000

    coincidencia por parte de string

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,12 +1,23 @@
 #include "holberton.h"
 /**
- * _strpbrk - Write a function that searches a string for any of a set of bytes.
- * @s:
- * @accept:
- *
- * Return:
+ * _strpbrk - Write a function that searches/
+ * a string for any of a set of bytes.
+ * @s: string
+ * @accept: part of string
+ * Return: 0 success
  */
 char *_strpbrk(char *s, char *accept)
 {
+	int a = 0, b;
 
+	while (s[a])
+	{
+		for (b = 0; accept[b]; b++)
+		{
+			if (accept[b] == s[a])
+				return (s + a);
+		}
+		a++;
+	}
+	return (0);
 }
