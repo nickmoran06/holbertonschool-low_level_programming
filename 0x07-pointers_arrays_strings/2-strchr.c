fd406f529a1f8f7d7053c3ca9449c9eeedd043fc
nickmoran06 <956@holbertonschool.com>
2019-07-08 20:14:33 +0000

    coincidencias luego un break

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,12 +1,26 @@
 #include "holberton.h"
 /**
  * _strchr - Write a function that locates a character in a string.
- * @s:
- * @c:
- *
- * Return:
+ * @s: pointer
+ * @c: character
+ * Return: a pointer to the first occurrence of the character/
+ * c in the string s, or NULL if the character is not found.
  */
 char *_strchr(char *s, char c)
 {
+	int n = 0;
 
+	while (s[n])
+	{
+		if (s[n] == c)
+		{
+			return (&s[n]);
+		}
+		else if (s[n] == 0)
+		{
+			return (0);
+		}
+		n++;
+	}
+	return (0);
 }
--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,5 +8,19 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
+	int a = 0, b;
 
+	while (s[a])
+	{
+		for (b = 0; accept[b]; b++)
+		{
+			if (s[a] == accept[b])
+				break;
+		}
+		if (!accept[b])
+			break;
+		a++;
+	}
+	return (a);
 }
+    
