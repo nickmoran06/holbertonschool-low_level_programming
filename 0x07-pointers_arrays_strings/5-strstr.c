b9f61304593350d8249854c9236d3390663fdc39
nickmoran06 <956@holbertonschool.com>
2019-07-08 21:59:23 +0000

    el 4 pero en reversa

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,12 +1,22 @@
 #include "holberton.h"
 /**
  * _strstr - Write a function that locates a substring.
- * @haystack:
- * @needle:
- *
- * Return:
+ * @haystack: string
+ * @needle: part of substring
+ * Return: 0 success
  */
 char *_strstr(char *haystack, char *needle)
 {
+	int a = 0, b;
 
+	while (needle[b])
+	{
+		for (a = 0; haystack[a]; a++)
+		{
+			if (needle[b] == haystack[a])
+				return (haystack + a);
+		}
+		b++;
+	}
+	return (0);
 }
