09e188a881cd391d2f4ffed097b48c972e265660
nickmoran06 <956@holbertonschool.com>
2019-07-18 15:54:45 +0000

    [update] concatenates wit restriction

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,13 +2,45 @@
 #include <stdlib.h>
 
 /**
+ * string_nconcat - a function that concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * @n: number of bytes of the second string
  *
- *
- *
- *
- * Return: 
+ * Return: a pointer to success, NULL if fails
  */
-
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	unsigned int i = 0;
+	unsigned int j = 0;
+	unsigned int l;
+	char *p;
 
+	if (!s1)
+		i = 0;
+	else
+		while (s1[i])
+			i++;
+
+	if (!s2)
+		j = 0;
+	else
+		while (s2[j])
+			j++;
+	if (j > n)
+		j = n;
+
+	p = malloc(i + j);
+	if (!p)
+		return (NULL);
+
+	for (l = 0; l < i; l++)
+		p[l] = s1[l];
+
+	for (l = 0; l < j; l++)
+		p[l + i] = s2[l];
+
+	p[l + i] = 0;
+
+	return (p);
 }
