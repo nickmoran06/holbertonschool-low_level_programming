fe7e731ecedfbbf77f341e8ad1e5b5faa65ee771
nickmoran06 <956@holbertonschool.com>
2019-07-22 22:17:06 +0000

    [update] header

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct - variable of the dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+#endif
