7d3d9de6233b558da350605abd62aba72fe025db
nickmoran06 <956@holbertonschool.com>
2019-07-23 01:47:48 +0000

    [update] a new dog header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
 
+typedef struct dog dog_t;
+
 /**
  * struct dog - variable of the dog
  * @name: name of the dog
@@ -13,9 +15,9 @@ struct dog
 	float age;
 	char *owner;
 };
-typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
