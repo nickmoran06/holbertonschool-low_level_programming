fe6df4c03ec2c563312ca8e847ecb677669dd653
nickmoran06 <956@holbertonschool.com>
2019-07-24 19:12:18 +0000

    [update] printing an array with a pointer of function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - a function that executes a function/
+ * given as a parameter on each element of an array.
+ *
+ * @size: The size of the array
+ * @array: Pointer to array
+ * @action: Is a pointer to the function you need to use
+ *
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array != NULL && action != NULL)
+	{
+		for (i = 0; i < size; i++)
+			(*action)(array[i]);
+	}
+}
