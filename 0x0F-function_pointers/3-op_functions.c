7220bf1b58a09003d73d3f6bd9f65c2ca9984109
nickmoran06 <nicolasmoranpulido@gmail.com>
2019-07-25 04:07:08 +0000

    [update] expansion of the code

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,7 +1,47 @@
 #include "3-calc.h"
 
+/**
+ * op_add - function to sum
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: integer
+ */
 int op_add(int a, int b)(return a + b);
+
+/**
+ * op_sub - function to substract
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: integer
+ */
 int op_sub(int a, int b)(return a - b);
+
+/**
+ * op_mul - function to multiplicate
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: integer
+ */
 int op_mul(int a, int b)(return a * b);
+
+/**
+ * op_div - function to divide
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: integer
+ */
 int op_div(int a, int b)(return a / b);
+
+/**
+ * op_mod - function to modulate
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: integer
+ */
 int op_mod(int a, int b)(return a % b);
+xsxc
