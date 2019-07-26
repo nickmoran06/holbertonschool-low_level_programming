b7e04b409e82475c4a71c4292e24f76b75d73733
nickmoran06 <956@holbertonschool.com>
2019-07-26 23:46:12 +0000

    [feature] in house

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - a function that prints anything
+ * @format:  is a list of types of arguments passed to the function
+ *
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+
+}
