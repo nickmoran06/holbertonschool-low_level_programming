3081efc91774e82d3aef6bef1f90d71cd6db708b
nickmoran06 <956@holbertonschool.com>
2019-08-01 21:03:17 +0000

    [fix] changing metodology

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,10 +13,10 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		if (h->str != NULL)
-			printf("[%d] %s\n", h->len, h->str);
-		else
+		if (h->str == NULL)
 			printf("[0] %p\n", h->str);
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		counter++;
 	}
