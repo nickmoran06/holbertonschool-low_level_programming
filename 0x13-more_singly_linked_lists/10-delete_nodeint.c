91b7eb8ece4a0705fb08e29d532557da32de6098
nickmoran06 <956@holbertonschool.com>
2019-08-07 00:27:33 +0000

    [update] changing conditionals

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -16,7 +16,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if ((!*head) || (!head))
 		return (-1);
 
-	if (index == 0)
+	if (index == '0')
 	{
 		next_node = (*head)->next;
 		free(*head);
