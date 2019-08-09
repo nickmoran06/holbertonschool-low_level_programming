7f5456f9972838160a10315a2b51b66676369fad
nickmoran06 <956@holbertonschool.com>
2019-08-09 21:47:18 +0000

    [feature] function decimal to binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,21 +1,21 @@
 #include "holberton.h"
 
 /**
- * print_binary -  a function that prints the binary representation of a number
- * @b: is pointing to a string of 0 and 1 chars
+ * print_binary - a function that prints the binary representation of a number
+ * @n: integer
  *
  * Return: void
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int cpy = n;
-	unsigned int count = (sizeof(unsigned long int));
-
-	for (count; count > 0; count >> 1)
+	if (n > 0)
 	{
-		if (count & cpy)
-			_putchar ('1');
-		else
-			_putchar ('0');
+		print_binary(n >> 1);
+		_putchar((n & 1) + '0');
 	}
+	else if (n == 0)
+	{
+		putchar (0 + '0');
+	}
+
 }
