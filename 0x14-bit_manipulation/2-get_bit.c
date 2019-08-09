cee22d5d8ddbb0a0affc2a6242ba252f45de60e4
nickmoran06 <nicolasmoranpulido@gmail.com>
2019-08-09 01:47:51 +0000

    [upload] more space to a variable

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,7 +9,7 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index > (sizeof(unsigned long int) * 2))
+	if (index > (sizeof(unsigned long int) * 4))
 		return (-1);
 	else
 		return (n >> index & 1);
