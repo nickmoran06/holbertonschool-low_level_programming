2c6ccbdac17154d53e43977835d171c94a3be92d
nickmoran06 <956@holbertonschool.com>
2019-08-09 22:18:32 +0000

    [update] removing 0 of beginning

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,6 +1,21 @@
 #include "holberton.h"
 
 /**
+ * print_binary_diff_0 - function to print binary different to 0
+ * @n: integer
+ *
+ * Return: void
+ */
+void print_binary_diff_0(unsigned long int n)
+{
+	if (n > 0)
+	{
+		print_binary_diff_0(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+}
+
+/**
  * print_binary - a function that prints the binary representation of a number
  * @n: integer
  *
@@ -8,14 +23,8 @@
  */
 void print_binary(unsigned long int n)
 {
-	if (n > 0)
-	{
-		print_binary(n >> 1);
-		_putchar((n & 1) + '0');
-	}
-	else if (n == 0)
-	{
-		putchar (0 + '0');
-	}
-
+	if (n != 0)
+		print_binary_diff_0(n);
+	else
+		_putchar('0');
 }
