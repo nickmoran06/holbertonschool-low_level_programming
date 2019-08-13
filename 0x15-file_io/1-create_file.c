fbc622e6b4164087e03fd9b83858b0cfe2895dfd
nickmoran06 <956@holbertonschool.com>
2019-08-13 22:06:09 +0000

    [update] change the loop

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,8 +20,8 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	while (text_content[counter])
-		counter++;
+	for (counter = 0; text_content[counter]; counter++)
+		;
 	if (text_content != NULL)
 	{
 		content = write(fd, text_content, counter);
