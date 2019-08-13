bc3703602af604533436b86e899a2605c1e9f93d
nickmoran06 <956@holbertonschool.com>
2019-08-13 22:16:05 +0000

    [update] changing metodology

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,18 +16,19 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
-	for (counter = 0; text_content[counter]; counter++)
-		;
-	if (text_content != NULL)
+	if (text_content)
 	{
+		while (text_content[counter])
+			counter++;
 		content = write(fd, text_content, counter);
 		if (content == -1)
 			return (-1);
 	}
+
 	close(fd);
 	return (1);
 }
