e8f61acdf4ff1fd5dedde1068e6f4b1a4ce94433
nickmoran06 <956@holbertonschool.com>
2019-09-15 21:06:17 +0000

    [feature] function to free a doubly linked list

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,8 +1,22 @@
 #include "lists.h"
 
 /**
+ * free_dlistint - function to free a doubly linked list list
+ * @head: pointer to the first node of the linked list
  *
- *
- *
- * Return:
+ * Return: void
  */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *next = NULL;
+
+	if (!head)
+		return;
+	while (head->next)
+	{
+		next = head->next;
+		free(head);
+		head = next;
+	}
+	free(head);
+}
