057fa0c7fe7e1612881f663891de26733aa637a3
nickmoran06 <956@holbertonschool.com>
2019-09-16 05:40:30 +0000

    [update] improvement of the previous node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,37 +11,39 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new = NULL, *current = NULL, *next = NULL;
+	dlistint_t *new = NULL, *prev = NULL, *next = NULL;
 	unsigned int counter = 0;
 
+	if (!(*h))
+		return (NULL);
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
 		return (NULL);
 	new->n = n;
-	current = *h;
 	if (idx != 0)
 	{
-		while (counter < (idx - 1) && current)
+		prev = *h;
+		while (counter < (idx - 1) && prev)
 		{
-			if (!current)
+			if (!prev)
 				return (NULL);
-			current = current->next;
+			prev = prev->next;
 			counter++;
 		}
-		next = current->next;
-		current->next = new;
+		next = prev->next;
+		prev->next = new;
+		new->prev = prev;
 		new->next = next;
-		if (new->next != NULL)
-		{
-			next->prev = new;
-		}
 	}
 	else
 	{
 		new->next = *h;
+		*h = next;
 		new->prev = NULL;
 		*h = new;
 	}
+	if (new->next != NULL)
+		(next)->prev = new;
 
 	return (new);
 }
