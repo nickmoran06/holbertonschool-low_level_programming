3354667c37a498539d627e6869a41456f24aa8a3
nickmoran06 <956@holbertonschool.com>
2019-09-16 04:40:14 +0000

    [feature] function to put a node in a specific position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,8 +1,47 @@
 #include "lists.h"
 
 /**
+ * insert_dnodeint_at_index - inserts a new node at a given position
  *
+ * @h: double pointer to the first node of the linked list
+ * @idx: index to insert the new node
+ * @n: integer data to enter into the new node
  *
- *
- * Return:
+ * Return: pointer to the new node
  */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new = NULL, *current = NULL, *next = NULL;
+	unsigned int counter = 0;
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	current = *h;
+	if (idx != 0)
+	{
+		while (counter < (idx - 1) && current)
+		{
+			if (!current)
+				return (NULL);
+			current = current->next;
+			counter++;
+		}
+		next = current->next;
+		current->next = new;
+		new->next = next;
+		if (new->next != NULL)
+		{
+			next->prev = new;
+		}
+	}
+	else
+	{
+		new->next = *h;
+		new->prev = NULL;
+		*h = new;
+	}
+
+	return (new);
+}
