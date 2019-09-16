203dc7a930abb30456b1024a7f521eb4129c1934
nickmoran06 <956@holbertonschool.com>
2019-09-16 00:33:51 +0000

    [feature] function to return a specific node

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,8 +1,30 @@
 #include "lists.h"
 
 /**
+ * get_dnodeint_at_index - function tofind a specific node of a linked list
  *
+ * @head: double pointer to the beginning node of the linked list
+ * @index: index of the node
  *
- *
- * Return:
+ * Return: pointer to the indexed node
  */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int counter = 0;
+	dlistint_t *node = NULL;
+
+	if (!head)
+		return (NULL);
+	node = head;
+	while (counter < index)
+	{
+		node = node->next;
+		if (!node)
+			return (NULL);
+		counter++;
+	}
+	if (index == 0)
+		return (head);
+
+	return (node);
+}
