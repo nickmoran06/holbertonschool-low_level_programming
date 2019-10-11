a93e6c12380445f911076a89da28db715bd8035f
nickmoran06 <956@holbertonschool.com>
2019-10-11 17:35:09 +0000

    [Fix] the amount of malloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,8 +19,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	hash_table->size = size;
-	hash_table->array = calloc(size, sizeof(hash_table_t));
-	if (!hash_table)
+	hash_table->array = calloc(size, sizeof(hash_table_t *));
+	if (!hash_table->array)
 	{
 		free(hash_table);
 		return (NULL);
