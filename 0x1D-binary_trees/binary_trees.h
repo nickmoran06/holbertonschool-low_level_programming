543b8d05c3eaf360bae23e69ef72b267180069ac
nickmoran06 <nicolasmoranpulido@gmail.com>
2019-12-03 05:18:57 +0000

    [Feature] library with the copy of the 0 exercise

--- 0x1D-binary_trees/binary_trees.h
+++ 0x1D-binary_trees/binary_trees.h
@@ -49,6 +49,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
 void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_node_1(binary_tree_t *parent, int value)
 
 
 #endif /* BINARY_TREES */
