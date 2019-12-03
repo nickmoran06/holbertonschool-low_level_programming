4bb815b12f457454eb789c51d888c2cc47e3b5e3
nickmoran06 <956@holbertonschool.com>
2019-12-03 19:03:54 +0000

    [Feature] modify functions and header

--- 0x1D-binary_trees/2-binary_tree_insert_right.c
+++ 0x1D-binary_trees/2-binary_tree_insert_right.c
@@ -1,31 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node_2 - function that creates a binary tree node
- *
- * @parent: pointer to the parent node of the node to create
- * @value:  is the value to put in the new node
- *
- * Return: pointer to the new node, or NULL on failure
- */
-binary_tree_t *binary_tree_node_2(binary_tree_t *parent, int value)
-{
-	binary_tree_t *new_node;
-
-	new_node = malloc(sizeof(binary_tree_t));
-	if (!new_node)
-		return (NULL);
-
-	new_node->parent = parent;
-	new_node->n = value;
-	new_node->left = NULL;
-	new_node->right = NULL;
-
-	return (new_node);
-}
-
-
-/**
  * binary_tree_insert_right - function that inserts a node
  * as the right-child of another node.
  *
@@ -41,7 +16,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	if (!parent)
 		return (NULL);
-	ins_node = binary_tree_node_2(parent, value);
+	ins_node = binary_tree_node(parent, value);
 	if (!ins_node)
 		return (NULL);
 
