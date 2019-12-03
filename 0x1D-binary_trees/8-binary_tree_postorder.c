ed02751995fb9367b6225279a7f464cd50d0a44f
nickmoran06 <956@holbertonschool.com>
2019-12-03 20:36:43 +0000

    [Feature] goes through post order transversal

---
+++ 0x1D-binary_trees/8-binary_tree_postorder.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - function that goes through
+ * a binary tree using pre-order traversal
+ *
+ * @tree: is a pointer to the root node of the tree to traverse
+ * @func: is a pointer to a function to call for each node
+ *
+ * Return: void
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree)
+	{
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
+		func(tree->n);
+	}
+}
