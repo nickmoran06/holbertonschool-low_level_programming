084901a48cdcaa6e6254e3588c332f06284260d3
nickmoran06 <nicolasmoranpulido@gmail.com>
2020-01-17 03:22:00 +0000

    [Fix] changing the the rest when declare the position on index more than the expected value

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -31,7 +31,7 @@ int binary_search(int *array, size_t size, int value)
 		if (array[middle] < value)
 			left = middle + 1;
 		else if (array[middle] > value)
-			right = middle - 1;
+			right = middle;
 		else
 			return (middle);
 		print_process_array(array + left, right + 1 - left);
