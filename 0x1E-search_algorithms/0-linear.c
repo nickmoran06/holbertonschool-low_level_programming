bd281b7f1dff6ff309311f94783295fbba2bd250
nickmoran06 <nicolasmoranpulido@gmail.com>
2020-01-17 00:57:40 +0000

    [Fix] changing the conditional to the print of process

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -19,7 +19,7 @@ int linear_search(int *array, size_t size, int value)
 	if (!array || size == 0)
 		return (-1);
 
-	for (i = 0; array[i] != '\0' && i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
